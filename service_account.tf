9b0ed5676c0fdb194fde0fb994a310e86fe6fbf3
Pawel Rusak <pjrusak@gmail.com>
2024-04-06 22:42:27 +0200

    Create two types of SAs with correct and incorrect role bindings

--- service_account.tf
+++ service_account.tf
@@ -4,14 +4,20 @@ locals {
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
-resource "google_service_account" "bucket_sa" {
-  account_id   = local.ksa_name
-  display_name = "CloudSql access Service Account"
+resource "google_service_account" "bucket_sa_wi" {
+  account_id   = "${local.ksa_name}-incorrect"
+  display_name = "CloudSql access Service Account with wrong IAM bindings"
+  project      = var.project_id
+}
+
+resource "google_service_account" "bucket_sa_wi_correct" {
+  account_id   = "${local.ksa_name}-correct"
+  display_name = "CloudSql access Service Account with correct IAM bindings"
   project      = var.project_id
 }
 
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
-module "project-iam-bindings" {
+module "project_iam_bindings" {
   source   = "terraform-google-modules/iam/google//modules/projects_iam"
   projects = [var.project_id, ]
   mode     = "authoritative"
@@ -19,27 +25,49 @@ module "project-iam-bindings" {
 
   bindings = {
     "roles/storage.admin" = [
-      "serviceAccount:${google_service_account.bucket_sa.email}",
+      "serviceAccount:${google_service_account.bucket_sa_wi.email}",
+      "serviceAccount:${google_service_account.bucket_sa_wi_correct.email}",
     ]
   }
 }
 
-module "service_account-iam-bindings" {
+module "bucket_sa_wi_iam_bindings" {
   source = "terraform-google-modules/iam/google//modules/service_accounts_iam"
 
-  service_accounts = [google_service_account.bucket_sa.email]
+  service_accounts = [google_service_account.bucket_sa_wi.email]
   project          = var.project_id
   mode             = "authoritative"
   #mode             = "additive"
 
   bindings = {
     "roles/iam.serviceAccountUser" = [
-      "serviceAccount:${google_service_account.bucket_sa.email}",
-      "serviceAccount:${var.project_id}.svc.id.goog[${local.k8s_namespace}/${local.ksa_name}]"
+      "serviceAccount:${google_service_account.bucket_sa_wi.email}",
+      "serviceAccount:${var.project_id}.svc.id.goog[${local.k8s_namespace}/${local.ksa_name}-incorrect]"
     ]
 
     "roles/iam.workloadIdentityUser" = [
-      "serviceAccount:${google_service_account.bucket_sa.email}",
+      "serviceAccount:${google_service_account.bucket_sa_wi.email}",
+    ]
+  }
+}
+
+module "bucket_sa_wi_correct_iam_bindings" {
+  source = "terraform-google-modules/iam/google//modules/service_accounts_iam"
+
+  service_accounts = [google_service_account.bucket_sa_wi.email]
+  project          = var.project_id
+  mode             = "authoritative"
+  #mode             = "additive"
+
+  bindings = {
+    "roles/iam.serviceAccountUser" = [
+      "serviceAccount:${google_service_account.bucket_sa_wi.email}",
+      "serviceAccount:${var.project_id}.svc.id.goog[${local.k8s_namespace}/${local.ksa_name}-correct]"
+    ]
+
+    "roles/iam.workloadIdentityUser" = [
+      "serviceAccount:${google_service_account.bucket_sa_wi.email}",
+      "serviceAccount:${var.project_id}.svc.id.goog[${local.k8s_namespace}/${local.ksa_name}-correct]"
     ]
   }
 }
