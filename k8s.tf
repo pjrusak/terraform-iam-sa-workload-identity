703e81a0c4bcfc2e7dbd9a1f886d647e9cf5f6f8
Pawel Rusak <pjrusak@gmail.com>
2024-04-08 09:16:27 +0200

    Provision KSAs and sample pods using those

--- k8s.tf
+++ k8s.tf
@@ -5,3 +5,98 @@ provider "kubernetes" {
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
+
+resource "kubernetes_service_account" "ksa_wi_principal" {
+  metadata {
+    name      = "${local.ksa_name}-principal"
+    namespace = local.k8s_namespace
+  }
+}
+
+resource "kubernetes_service_account" "ksa_wi_missing" {
+  metadata {
+    name      = "${local.ksa_name}-missing"
+    namespace = local.k8s_namespace
+
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.bucket_sa_wi_missing.email
+    }
+  }
+}
+
+resource "kubernetes_service_account" "ksa_wi_correct" {
+  metadata {
+    name      = "${local.ksa_name}-correct"
+    namespace = local.k8s_namespace
+
+    annotations = {
+      "iam.gke.io/gcp-service-account" = google_service_account.bucket_sa_wi_correct.email
+    }
+  }
+}
+
+resource "kubernetes_pod" "wi-bucket-ksa-missing-pod" {
+  metadata {
+    name      = "wi-${local.ksa_name}-missing-pod"
+    namespace = local.k8s_namespace
+  }
+
+  spec {
+    container {
+      image   = "google/cloud-sdk:slim"
+      name    = "wi-${local.ksa_name}-missing"
+      command = ["sleep", "infinity"]
+    }
+
+    node_selector = {
+      "iam.gke.io/gke-metadata-server-enabled" = "true"
+      "node_pool"                              = "pool-01"
+    }
+
+    service_account_name = kubernetes_service_account.ksa_wi_missing.id
+  }
+}
+
+resource "kubernetes_pod" "wi-bucket-ksa-correct-pod" {
+  metadata {
+    name      = "wi-${local.ksa_name}-correct-pod"
+    namespace = local.k8s_namespace
+  }
+
+  spec {
+    container {
+      image   = "google/cloud-sdk:slim"
+      name    = "wi-${local.ksa_name}-correct"
+      command = ["sleep", "infinity"]
+    }
+
+    node_selector = {
+      "iam.gke.io/gke-metadata-server-enabled" = "true"
+      "node_pool"                              = "pool-01"
+    }
+
+    service_account_name = kubernetes_service_account.ksa_wi_correct.id
+  }
+}
+
+resource "kubernetes_pod" "wi-bucket-ksa-principal-pod" {
+  metadata {
+    name      = "wi-${local.ksa_name}-principal-pod"
+    namespace = local.k8s_namespace
+  }
+
+  spec {
+    container {
+      image   = "google/cloud-sdk:slim"
+      name    = "wi-${local.ksa_name}-principal"
+      command = ["sleep", "infinity"]
+    }
+
+    node_selector = {
+      "iam.gke.io/gke-metadata-server-enabled" = "true"
+      "node_pool"                              = "pool-01"
+    }
+
+    service_account_name = kubernetes_service_account.ksa_wi_principal.id
+  }
+}
